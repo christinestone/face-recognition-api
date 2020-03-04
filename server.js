6ccb5cfb90c9dfdb4d29904ecbbe1572eec0d43d
Christine Stone <christinestone932@gmail.com>
2020-03-04 18:56:45 +1100

    use postgresql db to store users, use knex to connect to db, use bcrypt to hash passwords

--- server.js
+++ server.js
@@ -1,78 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'Joel',
-            email: 'joel@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Christine',
-            email: 'christine@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-};
-
 app.get('/', (req, res) => { res.send('it is working!') });
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt));
+app.get('/profile/:id', profile.handleProfile(db));
+app.put('/image', image.handleImage(db));
 
-app.post('/signin', (req, res) => {
-    const user = database.users.find(user => {
-        return user.email === req.body.email && user.password === req.body.password;
-    });
-    user ? res.json(user) : res.status(400).json('error logging in');
-});
-
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    database.users.push({
-        id: database.length,
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
-    });
-    res.json(database.users[database.users.length - 1]);
-});
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    const user = getUser(id);
-    user ? res.json(user) : res.status(404).json('no such user');
-});
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    const user = getUser(id);
-    if (user) {
-      user.entries++;
-      res.json(user.entries);
-    } else {
-        res.status(404).json('user not found');
-    }
-});
-
-const getUser = (id) => {
-    return database.users.find(user => {
-        return user.id === id;
-    });
-};
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+app.listen(port = process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${port}`);
 });
