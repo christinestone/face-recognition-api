6157820f734107c40e808b64dc8d66be92930db8
Christine Stone <christinestone932@gmail.com>
2020-03-05 18:34:04 +1100

    deploy

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'ba48d82805a84f47ace0e76352a3befd'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to connect to API'));
+};
+
 const handleImage = (db) => (req, res) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -9,5 +23,6 @@ const handleImage = (db) => (req, res) => {
 };
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 };
--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/profile/:id', profile.handleProfile(db));
 app.put('/image', image.handleImage(db));
+app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res)});
 
 app.listen(port = process.env.PORT || 3000, () => {
     console.log(`app is running on port ${port}`);
